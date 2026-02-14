b431dec66b2fe6f0241c50966f96332dc9a95d64
saravanalesnar <saravanalesnar@gmail.com>
2026-02-14 12:54:22 +0000

    3

--- send_mail.php
+++ send_mail.php
@@ -1,29 +1,28 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php';
+require 'vendor/autoload.php'; // This connects the library
 
 $mail = new PHPMailer(true);
 
 try {
+    // 1. Server Settings
     $mail->isSMTP();
     $mail->Host       = 'smtp.gmail.com';
     $mail->SMTPAuth   = true;
-    
-    // REPLACE THESE TWO LINES
     $mail->Username   = 'msakthivel.bear@gmail.com'; 
-    $mail->Password   = 'uqnpftqooqmnflot'; // Use an app password for Gmail
+    $mail->Password   = 'uqnpftqooqmnflot'; // Your 16-digit App Password
+    $mail->SMTPSecure = 'ssl';                  // Required for Gmail
+    $mail->Port       = 465;                    // Required for Gmail
 
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port       = 587;
-
+    // 2. Recipients
     $mail->setFrom('msakthivel.bear@gmail.com', 'Sakthi');
-    $mail->addAddress('msakthivel.bear@gmail.com', 'Sakthi'); // Change to her email later!
+    $mail->addAddress('msakthivel.bear@gmail.com'); // Change this to her email!
 
+    // 3. Content
     $mail->isHTML(true);
-    $mail->Subject = 'I Love You';
+    $mail->Subject = 'A Message from the Stars';
     $mail->Body    = 'Happy Valentine Day My Love. I love you so much. ❤️';
 
     $mail->send();
